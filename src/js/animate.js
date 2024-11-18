e7bbebad92b8616c80abb5e6c7b9536e3a59abc8
PC <romario777v8@gmail.com>
2024-11-18 17:40:40 +0200

    set anime library

---
+++ src/js/animate.js
@@ -0,0 +1,21 @@
+// animation for hero images
+document.addEventListener('DOMContentLoaded', () => {
+  const props = {
+    opacity: [1, 1],
+    duration: 2000,
+    easing: 'easeOutQuad',
+  };
+  // animation for .hero__image-caracal
+  anime({
+    targets: '.hero__image-caracal',
+    translateX: ['-200%', '-50%'],
+    ...props,
+  });
+
+  // animation for .hero__image-snake
+  anime({
+    targets: '.hero__image-snake',
+    translateX: ['100%', '0%'],
+    ...props,
+  });
+});
