f6ed22a516ae2a1eef3bf98428ab158cacde3309
PC <romario777v8@gmail.com>
2024-11-19 00:50:28 +0200

    styles

--- src/js/hero-paralax.js
+++ src/js/hero-paralax.js
@@ -5,13 +5,9 @@ const imageSnake = document.querySelector('.hero__image-snake');
 container.addEventListener('mousemove', function (event) {
   const isDesktop = window.innerWidth >= 1200;
 
-  const amplitude = isDesktop ? 20 : 10;
-
-  if (event.target.closest('.hero')) {
-    const moveX =
-      (event.clientX / window.innerWidth) * amplitude - amplitude / 2;
-    const moveY =
-      (event.clientY / window.innerHeight) * amplitude - amplitude / 2;
+  if (event.target.closest('.hero') && isDesktop) {
+    const moveX = (event.clientX / window.innerWidth) * 20 - 10;
+    const moveY = (event.clientY / window.innerHeight) * 20 - 10;
 
     imageCaracal.style.transform = `translateX(-50%) translate(${moveX}px, ${moveY}px)`;
     imageSnake.style.transform = `translate(${-moveX}px, ${-moveY}px)`;
