2a7b6374a7439c3fa9f2ab8ed4c9259963c5bb05
PC <romario777v8@gmail.com>
2024-11-19 11:47:28 +0200

    fix

--- src/js/animate.js
+++ src/js/animate.js
@@ -3,26 +3,24 @@
   | animation for hero images
   |============================
 */
-document.addEventListener('DOMContentLoaded', () => {
-  const props = {
-    opacity: [1, 1],
-    duration: 3000,
-    delay: 500,
-    easing: 'easeOutQuad',
-  };
-  // animation for .hero__image-caracal
-  anime({
-    targets: '.hero__image-caracal',
-    translateX: ['-200%', '-50%'],
-    ...props,
-  });
+const props = {
+  opacity: [1, 1],
+  duration: 3000,
+  delay: 500,
+  easing: 'easeOutQuad',
+};
+// animation for .hero__image-caracal
+anime({
+  targets: '.hero__image-caracal',
+  translateX: ['-200%', '-50%'],
+  ...props,
+});
 
-  // animation for .hero__image-snake
-  anime({
-    targets: '.hero__image-snake',
-    translateX: ['100%', '0%'],
-    ...props,
-  });
+// animation for .hero__image-snake
+anime({
+  targets: '.hero__image-snake',
+  translateX: ['100%', '0%'],
+  ...props,
 });
 
 /**
