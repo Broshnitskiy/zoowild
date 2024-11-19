0f9dabdfe0bd1cc8009d61db1d3e00e59fb3e39c
PC <romario777v8@gmail.com>
2024-11-19 16:47:51 +0200

    change anim

--- src/js/animate.js
+++ src/js/animate.js
@@ -3,25 +3,27 @@
   | animation for hero images
   |============================
 */
-const props = {
-  opacity: [1, 1],
-  duration: 3000,
-  delay: 500,
-  easing: 'easeOutQuad',
-};
-// animation for .hero__image-caracal
-anime({
-  targets: '.hero__image-caracal',
-  translateX: ['-200%', '-50%'],
-  ...props,
-});
 
-// animation for .hero__image-snake
-anime({
-  targets: '.hero__image-snake',
-  translateX: ['100%', '0%'],
-  ...props,
-});
+window.onload = () => {
+  const props = {
+    opacity: [1, 1],
+    duration: 3000,
+    easing: 'easeOutQuad',
+  };
+  // animation for .hero__image-caracal
+  anime({
+    targets: '.hero__image-caracal',
+    translateX: ['-200%', '-50%'],
+    ...props,
+  });
+
+  // animation for .hero__image-snake
+  anime({
+    targets: '.hero__image-snake',
+    translateX: ['100%', '0%'],
+    ...props,
+  });
+};
 
 /**
   |============================
