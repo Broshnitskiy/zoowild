2ac91e3769e04ffd3159556db772ba35e2bee878
PC <romario777v8@gmail.com>
2024-12-03 18:04:10 +0200

    add location

--- src/js/quick-btns.js
+++ src/js/quick-btns.js
@@ -9,23 +9,30 @@ const modalRefs = {
     modal: document.querySelector('[data-contacts-popup]'),
     close: document.querySelector('[data-contacts-popup-close]'),
   },
+  location: {
+    trigger: document.querySelector('[data-location-btn]'),
+    modal: document.querySelector('[data-location-popup]'),
+    close: document.querySelector('[data-location-popup-close]'),
+  },
 };
 
 Object.values(modalRefs).forEach(({ trigger, modal, close }) => {
   trigger.addEventListener('click', () => {
-    Object.values(modalRefs).forEach(({ modal: inModal }) => {
-      if (!inModal.classList.contains('is-hidden') && modal !== inModal) {
-        inModal.classList.add('is-hidden');
-      }
-    });
-
     modal.classList.toggle('is-hidden');
 
     const closeHandler = () => {
       modal.classList.add('is-hidden');
+      document.removeEventListener('click', outsideClickHandler);
       close.removeEventListener('click', closeHandler);
     };
 
+    const outsideClickHandler = e => {
+      if (!modal.contains(e.target) && !trigger.contains(e.target)) {
+        closeHandler();
+      }
+    };
+
     close.addEventListener('click', closeHandler);
+    document.addEventListener('click', outsideClickHandler);
   });
 });
