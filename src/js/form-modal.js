3c2ccf71ab3d2f938df4e5244dab88f2728ec2d7
PC <romario777v8@gmail.com>
2024-12-04 13:01:28 +0200

    add form

--- src/js/form-modal.js
+++ src/js/form-modal.js
@@ -2,8 +2,10 @@ const openFormBtnRef = document.querySelectorAll('[data-modal-form-open]');
 const closeFormBtnRef = document.querySelector('[data-close-form-modal]');
 const modalFormRef = document.querySelector('[data-form-modal]');
 const contactsPopupRef = document.querySelector('[data-contacts-popup]');
+const formRef = document.querySelector('.form');
 
 openFormBtnRef.forEach(btn => btn.addEventListener('click', openModal));
+formRef.addEventListener('submit', handleFormSubmit);
 
 function openModal() {
   if (modalFormRef.classList.contains('is-hidden')) {
@@ -37,4 +39,26 @@ function handleOverlayClick(e) {
   if (e.target === e.currentTarget) {
     closeModal();
   }
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData.entries());
+
+  // console.log('Зібрані дані:', data);
+
+  closeModal();
+
+  if (contactsPopupRef.classList.contains('is-hidden')) {
+    contactsPopupRef.classList.remove('is-hidden');
+  }
+
+  setTimeout(() => {
+    if (!contactsPopupRef.classList.contains('is-hidden')) {
+      contactsPopupRef.classList.add('is-hidden');
+    }
+    document.body.style.overflowY = 'visible';
+  }, 7000);
 }
