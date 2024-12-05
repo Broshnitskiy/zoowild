c1ace50573eecef39c54ec39ab3b013c0da8cafb
PC <romario777v8@gmail.com>
2024-12-05 10:21:29 +0200

    fix

--- src/js/quick-btns.js
+++ src/js/quick-btns.js
@@ -9,11 +9,12 @@ const modalRefs = {
     modal: document.querySelector('[data-contacts-popup]'),
     close: document.querySelector('[data-contacts-popup-close]'),
   },
-  location: {
-    trigger: document.querySelector('[data-location-btn]'),
-    modal: document.querySelector('[data-location-popup]'),
-    close: document.querySelector('[data-location-popup-close]'),
-  },
+};
+
+const locationRef = {
+  trigger: document.querySelector('[data-location-btn]'),
+  modal: document.querySelector('[data-location-popup]'),
+  close: document.querySelector('[data-location-popup-close]'),
 };
 
 Object.values(modalRefs).forEach(({ trigger, modal, close }) => {
@@ -36,3 +37,33 @@ Object.values(modalRefs).forEach(({ trigger, modal, close }) => {
     document.addEventListener('click', outsideClickHandler);
   });
 });
+
+locationRef.trigger.addEventListener('click', openLocationModal);
+
+function openLocationModal() {
+  locationRef.modal.classList.remove('is-hidden');
+  locationRef.modal.addEventListener('click', handleOverlayClick);
+  document.body.style.overflowY = 'hidden';
+  locationRef.close.addEventListener('click', closeLocationModal);
+  document.addEventListener('keydown', handleKeyPress);
+}
+
+function closeLocationModal() {
+  locationRef.modal.classList.add('is-hidden');
+  locationRef.modal.removeEventListener('click', handleOverlayClick);
+  document.body.style.overflowY = 'visible';
+  document.removeEventListener('keydown', handleKeyPress);
+  locationRef.close.removeEventListener('click', closeLocationModal);
+}
+
+function handleKeyPress(event) {
+  if (event.key === 'Escape') {
+    closeLocationModal();
+  }
+}
+
+function handleOverlayClick(e) {
+  if (e.target === e.currentTarget) {
+    closeLocationModal();
+  }
+}
